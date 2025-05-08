a51789bbae200ac7779ac0e6a78a3d2a65e9fda7
mudassir <mudassirkhantailor@gmail.com>
2025-05-08 10:44:04 +0530

    Added "Feature-Created HTTP Server Using NodeJS"

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const fs = require("fs");
+const http = require("http");
 fs.writeFileSync("hello.txt", "Welocome to Node Js");
+function reqListener(req, res) {
+  console.log(req);
+  //   console.log(res);
+}
+const server = http.createServer(reqListener);
+
+server.listen(3000, () => {
+  console.log("Server Running On Port  3000");
+});
