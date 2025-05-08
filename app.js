6589253ce166e7cb99e9ce151d7b83bbc284520f
mudassir <mudassirkhantailor@gmail.com>
2025-05-08 11:06:24 +0530

    Added "Functionality-Sending Response"

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const fs = require("fs");
+// const fs = require("fs");
 const http = require("http");
-fs.writeFileSync("hello.txt", "Welocome to Node Js");
+// fs.writeFileSync("hello.txt", "Welocome to Node Js");
 function reqListener(req, res) {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
-
+  //   console.log(req.url);
+  //   console.log(req.method);
+  //   console.log(req.headers);
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>NodeJs</title></head>");
+  res.write("<body><h1>NodeJs response From Mudassir Khan</h1></body>");
+  res.write("</html>");
+  res.end();
   //   console.log(res);
 }
 const server = http.createServer(reqListener);
